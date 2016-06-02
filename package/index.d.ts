7db748091ccd9b87d30aea08feb27d4762c6303c
keeema <rut.tomas@hotmail.com>
2016-06-02 23:18:37 +0200

    Transformation to bobril-build

--- package/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { ICursor } from 'fun-model';
-export declare let init: (cursor?: ICursor<any>) => (m: any, p: any) => void;
--- package/rows.d.ts
+++
@@ -1,6 +0,0 @@
-import * as b from 'bobril';
-import * as row from './row';
-export interface IData {
-    rows: row.IData[];
-}
-export declare let create: (data?: IData, children?: boolean | string | b.IBobrilNodeWithTag | b.IBobrilNodeWithComponent | b.IBobrilNodeWithChildren | b.IBobrilChildArray) => b.IBobrilNodeWithTag | b.IBobrilNodeWithComponent | b.IBobrilNodeWithChildren;
