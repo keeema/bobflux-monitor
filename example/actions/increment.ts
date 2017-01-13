dd7e5ee2714c7ddc2ea5fd3573b1e105476cc13f
keeema <trut.cz@gmail.com>
2017-01-13 09:00:43 +0100

    Fixed example

--- example/actions/increment.ts
+++ example/actions/increment.ts
@@ -4,6 +4,6 @@ import { appCursor } from '../state';
 export const increment = f.createAction(appCursor, state => f.shallowCopy(state, copy => {
     copy.counter++;
     copy.date = new Date(Date.now());
-    copy.popup = () => alert(`Current value is ${copy.counter}.`);
+    copy.popup = () => alert('Popup opened by some data-closed function.');
     copy.history = [...state.history, copy.counter];
 }));
